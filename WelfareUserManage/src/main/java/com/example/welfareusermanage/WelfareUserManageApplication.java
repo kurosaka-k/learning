653e972519ddaf68531c59b5cb6de370734c1559
kurosaka <kurosaka-k@aisel.ne.jp>
2023-07-05 09:47:18 +0900

    new project

---
+++ WelfareUserManage/src/main/java/com/example/welfareusermanage/WelfareUserManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.welfareusermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelfareUserManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelfareUserManageApplication.class, args);
+	}
+
+}
